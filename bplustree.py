358ac50143c3f2647fe427190e53a538d070f6bc
Sam Whitehall <me@samwhitehall.com>
2013-05-28 15:23:21 +0100

    Basic insertion (case 1)
    i.e. when key/record pair is the first being added

--- bplustree.py
+++ bplustree.py
@@ -1,14 +1,24 @@
 class Node:
-    def __init__(self, values=None, records=None):
-        self.values = values or []
-        self.records = records or []
+    def __init__(self, keys=None, children=None):
+        self.keys = keys or []
+        self.children = children or []
 
 class NonLeaf(Node):
     pass
 
 class Leaf(Node):
-    pass
+    def __init__(self, records, keys_per_node):
+        blanks = keys_per_node - len(records)
+
+        self.keys = [record[0] for record in records] + ([None] * blanks)
+        self.children = [record[1] for record in records] + ([None] * blanks)
 
 class Tree:
-    def __init__(self):
-        self.root = NonLeaf()
+    def __init__(self, keys_per_node=4):
+        self.keys_per_node = keys_per_node
+        self.root = NonLeaf(keys=[None]*keys_per_node, 
+                            children=[None]*(keys_per_node+1) )
+
+    def insert(self, record):
+        self.root.keys[0] = record[0]
+        self.root.children[0] = Leaf([record], self.keys_per_node)
